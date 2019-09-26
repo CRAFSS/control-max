88f6a700a54fd5cc9c9ef98dda811660ebeb92d5
rafabra988 <44869209+rafabra988@users.noreply.github.com>
2019-09-26 10:40:07 -0300

    26/09

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'modal', loadChildren: './modal/modal.module#ModalPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
+  { path: 'novogasto', loadChildren: './novogasto/novogasto.module#NovogastoPageModule' },
+  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'sobre', loadChildren: './sobre/sobre.module#SobrePageModule' },
+  { path: 'ajustes', loadChildren: './ajustes/ajustes.module#AjustesPageModule' },
+  { path: 'historico', loadChildren: './historico/historico.module#HistoricoPageModule' },
+  { path: 'cadastro', loadChildren: './cadastro/cadastro.module#CadastroPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
