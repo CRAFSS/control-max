902cdb24e1f95dd1f4b4288c58309ef38efc842f
JBragaV <44869203+JBragaV@users.noreply.github.com>
2019-09-27 12:00:56 -0300

    Add files via upload

---
+++ modal/modal.page.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { StorageService, registro } from '../services/storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.page.html',
+  styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit{
+
+  registros: registro[] = [];
+  parar:any;
+  total:number;
+  debito:boolean;
+  cretdito:boolean;
+
+  @ViewChild('mylist', {static: false})mylist: IonList;
+  
+  constructor(private modalController: ModalController,private router:Router , private storageService: StorageService, private toastController: ToastController, private storage:Storage) {
+  }
+  
+  //ionViewWillEnter()
+  ngOnInit(){
+    this.listarRegistros();
+    this.parar = setInterval(() => {
+      this.msg();
+    }, 1);
+  }
+
+  msg(){
+    this.storage.get('meus-registros').then(test => {
+      if(test){
+        if(this.registros.length !== 0){
+          document.getElementById("test").style.display = "none";
+        }else{
+          document.getElementById("test").style.display = "block";
+        }
+      }
+    })
+  }
+
+  fechar() {
+    clearInterval(this.parar)
+    this.modalController.dismiss();
+  }
+
+  listarRegistros(){
+    console.log("Listando Registros")
+    this.storageService.listaRegistros().then(registro =>{
+      this.registros = registro;
+      this.total = 0;
+      for(let i = 0; i < this.registros.length; i++){
+        console.log(this.registros[i].tipo)
+        console.log("Essa é a verificação das despesas "+this.registros[i].tipo === "d")
+        console.log("Essa é a verificação das receitas "+this.registros[i].tipo === "g")
+      }
+      if(this.registros){
+        console.log("Entrei no if")
+        this.registros.forEach(element => {
+          console.log("Listando os elementos")
+          if(element.tipo == "g"){
+            console.log("Estou no G")
+            this.total += element.valor;
+          }else{
+            console.log("Estou no R")
+            this.total -= element.valor;
+          }
+        });
+        this.storage.set("total", this.total)
+      } 
+    }
+    
+    );
+  }
+
+  deletarRegistro(registro:registro){
+    this.storageService.deletarRegistro(registro.id).then(registro=>{
+      this.showToast('Compra Deletada!');
+      this.mylist.closeSlidingItems();
+      this.listarRegistros();
+    })
+  }
+
+  async showToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  chahistorico(){
+    clearInterval(this.parar)
+    this.modalController.dismiss();
+    this.router.navigateByUrl('historico')
+  }
+
+}
