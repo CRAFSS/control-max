227f56fd96bc3c9d6897e819791e537e408984e1
JoRPM <44869196+JoRPM@users.noreply.github.com>
2019-11-07 11:35:34 -0300

    Update settings.page.ts

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -10,9 +10,7 @@ export class SettingsPage implements OnInit {
 
   constructor(public modalController: ModalController, private nav:NavController) { }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() { }
 // função de botão de voltar página
   fechar() {
     this.nav.pop()
